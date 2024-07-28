80e73f4d266330c9adfb89d37c58cce81d6f3f0b
Mohammad Hirbawi <hirbawi198@gmail.com>
2024-07-28 18:48:59 +0300

    Fix auth not defined in Logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase';
 
 const Logout = () => {
     const { currentUser } = useAuth();
