e0468a1baa452f4050c9460f0d28e33e475f6715
Mohammad Hirbawi <hirbawi198@gmail.com>
2024-07-28 18:42:32 +0300

    Add React Router and improve UI with CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import AddNote from './components/AddNote';
@@ -7,13 +8,32 @@ import NotesList from './components/NotesList';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Collaborative Note Taking App</h1>
-            <Login />
-            <Signup />
-            <AddNote />
-            <NotesList />
-        </div>
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">Signup</Link>
+                        </li>
+                        <li>
+                            <Link to="/add-note">Add Note</Link>
+                        </li>
+                        <li>
+                            <Link to="/notes">Notes</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/add-note" component={AddNote} />
+                    <Route path="/notes" component={NotesList} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
